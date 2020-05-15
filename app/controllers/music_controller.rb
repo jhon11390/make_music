4bee61186b25c741e7398162519978d54e2e8dac
jhon11390 <meljhon2@gmail.com>
2020-05-15 03:25:51 -0500

    Crea los controladores y vistas de artistas y albunes

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -2,4 +2,9 @@ class MusicController < ApplicationController
   def index
     @artistas = Artist.all
   end
+
+  def albums
+    @artist = Artist.find(params[:id])
+    @albums = @artist.albums
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'music/index'
+  get '/artists', to: 'music#index' 
+  get '/artists/:id', to: 'music#albums'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
