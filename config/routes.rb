5f7aef4246f0f5c8607b6ae285984c83ebbe6376
jhon11390 <meljhon2@gmail.com>
2020-05-15 20:57:47 -0500

    Arregla problema de rutas  segunda solucion

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/artists', to: 'music#index'
-  get '/artists/:id', to: 'music#show', as: "artista"
+  get '/artists/:id', to: 'music#show', as: "artist"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
